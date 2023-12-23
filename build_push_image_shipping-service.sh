0d9efff351b000cd34605d2255487bb86fb83a58
ihyaksm <ihyaksm@gmail.com>
2023-12-23 23:33:25 +0700

    shipping-service

---
+++ build_push_image_shipping-service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# build Docker image dari berkas Dockerfile
+docker build -t ghcr.io/ihyaksm/shipping-service:latest .
+
+# login ke github
+echo $CR_PAT | docker login ghcr.io -u ihyaksm --password-stdin
+
+# push image ke github packages
+docker push ghcr.io/ihyaksm/shipping-service:latest
